171ae1b400be9ad5a0ee379070759d0417e1fc60
JupiterSky11 <jupitersky11@gmail.com>
2016-12-06 17:02:47 -0500

    Update gameEngine.py

--- gameEngine.py
+++ gameEngine.py
@@ -6,23 +6,27 @@ import math
 import sys
 
 windowRunning = 1
+windowSizeX = 1200
+windowSizeY = 800
 
+velReset = 0
 upPress = 0
 downPress = 0
 leftPress = 0
 rightPress = 0
 
 plyrVelX = 0
-plryVelY = 0
-plryX = 30
+plyrVelY = 0
+plyrX = 30
 plyrY = 30
 
 BLACK = 0,0,0
 
 
 
+screen = pygame.display.set_mode([windowSizeX, windowSizeY])
 pygame.display.set_caption("Movable Square")
-screen = pygame.display.set_mode([1020, 600])
+
 
 while windowRunning == 1:
 
@@ -33,58 +37,71 @@ while windowRunning == 1:
 
     # UP
 
-    if pygame.event == pygame.KEYDOWN and pygame.event.key == pygame.K_w:
+    if event.type == pygame.KEYDOWN and event.key == pygame.K_w:
         upPress = 1
-    if pygame.event == pygame.KEYUP and pygame.event.key == pygame.K_w:
+    if event.type == pygame.KEYUP and event.key == pygame.K_w:
         upPress = 0
 
 
     # DOWN
 
-    if pygame.event == pygame.KEYDOWN and pygame.event.key == pygame.K_s:
+    if event.type == pygame.KEYDOWN and event.key == pygame.K_s:
         downPress = 1
-    if pygame.event == pygame.KEYUP and pygame.event.key == pygame.K_s:
+    if event.type == pygame.KEYUP and event.key == pygame.K_s:
         downPress = 0
 
 
     # LEFT
 
-    if pygame.event == pygame.KEYDOWN and pygame.event.key == pygame.K_a:
+    if event.type == pygame.KEYDOWN and event.key == pygame.K_a:
         leftPress = 1
-    if pygame.event == pygame.KEYUP and pygame.event.key == pygame.K_a:
+    if event.type == pygame.KEYUP and event.key == pygame.K_a:
         leftPress = 0
 
 
     #RIGHT
 
-    if pygame.event == pygame.KEYDOWN and pygame.event.key == pygame.K_d:
+    if event.type == pygame.KEYDOWN and event.key == pygame.K_d:
         rightPress = 1
-    if pygame.event == pygame.KEYUP and pygame.event.key == pygame.K_d:
+    if event.type == pygame.KEYUP and event.key == pygame.K_d:
         rightPress = 0
 
+    #STOP
 
+    if event.type == pygame.KEYDOWN and event.key == pygame.K_r:
+        velReset = 1
+    if event.type == pygame.KEYUP and event.key == pygame.K_r:
+        velReset = 0
+
+
+
+    if velReset == 1:
+        plyrVelX = 0
+        plyrVelY = 0
 
     if upPress == 1:
-        plyrVelX += 1
+        plyrVelY -= 0.0005
 
     if downPress == 1:
-        plyrVelX -= 0.01
+        plyrVelY += 0.0005
 
     if leftPress == 1:
-        plryVelY -= 0.01
+        plyrVelX -= 0.0005
 
     if rightPress == 1:
-        plryVelY += 0.01
+        plyrVelX += 0.0005
 
 
 
-    plryX += plyrVelX
-    plyrY += plryVelY
+    plyrX += plyrVelX
+    plyrY += plyrVelY
 
     screen.fill(BLACK)
 
-    pygame.draw.rect(screen, [0,255,0], [plryX, plyrY, 10, 10])
+    pygame.draw.rect(screen, [0,255,0], [plyrX, plyrY, 10, 10])
 
     pygame.display.flip()
+
+    print(plyrY,plyrX)
 
 
