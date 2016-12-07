d9c6c26829ca7fa8f12049d919f5d92586695d45
JupiterSky11 <jupitersky11@gmail.com>
2016-12-06 20:55:59 -0500

    Update gameEngine.py

--- gameEngine.py
+++ gameEngine.py
@@ -6,7 +6,7 @@ import math
 import sys
 
 windowRunning = 1
-windowSizeX = 1200
+windowSizeX = 1280
 windowSizeY = 800
 
 velReset = 0
@@ -17,8 +17,8 @@ rightPress = 0
 
 plyrVelX = 0
 plyrVelY = 0
-plyrX = 30
-plyrY = 30
+plyrX = windowSizeX / 2
+plyrY = windowSizeY / 2
 
 facing = 0
 
@@ -75,7 +75,7 @@ while windowRunning == 1:
     if event.type == pygame.KEYUP and event.key == pygame.K_r:
         velReset = 0
 
-    
+
     #Facing
     if upPress == 1 and rightPress == 1:
         facing = 1
@@ -101,7 +101,7 @@ while windowRunning == 1:
     elif rightPress == 1:
         facing = 2
 
-    
+
     #Velocity
     if velReset == 1:
         plyrVelX = 0
@@ -123,37 +123,37 @@ while windowRunning == 1:
     #Posision
     plyrX += plyrVelX
     plyrY += plyrVelY
-    
+
     #Refreshing the screen
     screen.fill(BLACK)
 
     #Drawing player
-    pygame.draw.rect(screen, [0,255,0], [plyrX, plyrY, 10, 10])
+    pygame.draw.rect(screen, [0,255,0], [plyrX, plyrY, 11, 11])
 
     #Drawing gun thing
     if facing == 0:
-        pygame.draw.line(screen, [0,200,0], [plyrX + 4, plyrY + 5],[plyrX + 4, plyrY + -10], 2)
+        pygame.draw.line(screen, [0,200,0], [plyrX + 5, plyrY + 5],[plyrX + 5, plyrY + -10], 1)
 
-    if facing == 1:
-        pygame.draw.line(screen, [0,200,0], [plyrX + 4, plyrY + 4],[plyrX + 16, plyrY + -7], 2)
+    elif facing == 1:
+        pygame.draw.line(screen, [0,200,0], [plyrX + 5, plyrY + 5],[plyrX + 17.5, plyrY + -7.5], 1)
 
-    if facing == 2:
-        pygame.draw.line(screen, [0,200,0], [plyrX + 4, plyrY + 4],[plyrX + 20, plyrY + 4], 2)
+    elif facing == 2:
+        pygame.draw.line(screen, [0,200,0], [plyrX + 5, plyrY + 5],[plyrX + 20, plyrY + 5], 1)
 
-    if facing == 3:
-        pygame.draw.line(screen, [0,200,0], [plyrX + 4, plyrY + 4],[plyrX + 20, plyrY + 4], 2)
+    elif facing == 3:
+        pygame.draw.line(screen, [0,200,0], [plyrX + 5, plyrY + 5],[plyrX + 17.5, plyrY + 17.5], 1)
 
-    if facing == 4:
-        pygame.draw.line(screen, [0,200,0], [plyrX + 4, plyrY + 4],[plyrX + 20, plyrY + 4], 2)
+    elif facing == 4:
+        pygame.draw.line(screen, [0,200,0], [plyrX + 5, plyrY + 5],[plyrX + 5, plyrY + 20], 1)
 
-    if facing == 5:
-        pygame.draw.line(screen, [0,200,0], [plyrX + 4, plyrY + 4],[plyrX + 20, plyrY + 4], 2)
+    elif facing == 5:
+        pygame.draw.line(screen, [0,200,0], [plyrX + 5, plyrY + 5],[plyrX + -7.5, plyrY + 17.5], 1)
 
-    if facing == 6:
-        pygame.draw.line(screen, [0,200,0], [plyrX + 4, plyrY + 4],[plyrX + 20, plyrY + 4], 2)
+    elif facing == 6:
+        pygame.draw.line(screen, [0,200,0], [plyrX + 5, plyrY + 5],[plyrX + -10, plyrY + 5], 1)
 
-    if facing == 7:
-        pygame.draw.line(screen, [0,200,0], [plyrX + 4, plyrY + 4],[plyrX + 20, plyrY + 4], 2)
+    elif facing == 7:
+        pygame.draw.line(screen, [0,200,0], [plyrX + 5, plyrY + 5],[plyrX + -7.5, plyrY + -7.5], 1)
 
 
     #Displaying the changes
