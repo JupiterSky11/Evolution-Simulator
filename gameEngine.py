2a08451278b295d7011585e2b962c607c77d13eb
JupiterSky11 <jupitersky11@gmail.com>
2017-05-07 22:08:33 -0400

    Update gameEngine.py

--- gameEngine.py
+++ gameEngine.py
@@ -1,1 +1,13 @@
+import pygame
+import os
+import time
 
+
+winRun = 1
+winWidth = 100
+winHeight = 100
+gameName = "Make Games Great Again"
+
+
+screen = pygame.display.set_mode((winWidth, winHeight))
+pygame.display.set_caption("gameName")
